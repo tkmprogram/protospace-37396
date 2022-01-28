39470cf2b5b2b5af4e838fc0a54a0e5b10093da7
tkmprogram <takumi.kojo.2@gmail.com>
2022-01-28 22:33:20 +0900

    issue解消

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,8 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, except: [:index, :show]
+
+  
   def index
     @prototypes = Prototype.all
   end
@@ -53,6 +56,12 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
   
