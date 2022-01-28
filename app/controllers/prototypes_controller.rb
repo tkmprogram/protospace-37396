516b920b3b201e9d72b8363d44f8fb4018ddd055
tkmprogram <takumi.kojo.2@gmail.com>
2022-01-28 16:23:13 +0900

    edit修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -32,6 +32,12 @@ class PrototypesController < ApplicationController
 
   def update
     @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+    if @prototype.save
+      redirect_to prototype_path
+    else
+      render :edit
+    end
   end
 
   def destroy
